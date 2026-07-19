8d10d56cc3b570e8cf3faa52d6b08328485885f7
SSG <sumedhsinghg@gmail.com>
2026-07-19 23:37:52 +0530

    CHAPTER 1: Initial, UI, modetoggle, and database setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
+export default function Home() {
+  return (
+    <div>Hale hula haha
+      <ModeToggle/>
+    </div>
+
+  );
+}
